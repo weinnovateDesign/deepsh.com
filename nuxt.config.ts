3200ac90c8363ef192ab3a16b49be1c0ec3e7ef5
bigdatacloudapi <deep@bigdatacloud.com>
2024-06-19 10:11:50 +0930

    added nuxtContent

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/content", "@nuxt/image-edge", "@nuxt/image"],
+	modules: ["@nuxt/content", "@nuxt/image-edge", "@nuxt/image", "@nuxthq/studio"],
 	css: ["~/assets/css/main.css"],
 	postcss: {
 		plugins: {
